e3731e075e3a4ccd86679b5406181ee1cf857f3f
Roman Sorokovikov <r.sorokovikov@ispsystem.com>
2023-10-27 00:42:45 +0800

    Fix task 6

--- school/recursion/task_6.py
+++ school/recursion/task_6.py
@@ -1,8 +1,12 @@
 def print_even_index_elements(list_: list) -> None:
 
-    if len(list_) < 2:
+    if len(list_) == 0:
         return
 
     print(list_.pop(0))
+
+    if len(list_) == 0:
+        return
+
     list_.pop(0)
     print_even_index_elements(list_)
--- school/recursion/test_task_6.py
+++ school/recursion/test_task_6.py
@@ -15,4 +15,10 @@ class TestPrint(TestCase):
             print_even_index_elements([32, "hello", "world", 1337, 21, "foo", "bar", 322])
         self.assertEqual("32\nworld\n21\nbar\n", output.getvalue())
 
+        output = StringIO()
+
+        with redirect_stdout(output):
+            print_even_index_elements([100, "foo", "bar"])
+        self.assertEqual("100\nbar\n", output.getvalue())
+
         output.close()
